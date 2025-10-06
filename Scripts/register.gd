42877680275a3bb7d97ca1fd0f42b4c622515cac
22DP2TSili <131154623+22DP3TSilins@users.noreply.github.com>
2025-10-06 08:45:29 +0300

    Delete Scripts/register.gd

--- Scripts/register.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_register_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
-
-func _on_cancel_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
